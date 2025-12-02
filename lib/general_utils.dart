2cdd8d283007ff6eef88e3c02dbd5fe9967e3c53
John Kalu <jhkalu@gmail.com>
2025-12-02 14:01:55 -0500

    feat: exported new classes

--- lib/general_utils.dart
+++ lib/general_utils.dart
@@ -1,5 +1,3 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/enum_extensions.dart';
+export 'src/format.dart';
+export 'src/text_scale_box.dart';
