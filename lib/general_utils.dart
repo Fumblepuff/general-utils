449fa6bb87380cb0d8cffc12ac7200ef13fa2097
John Kalu <jhkalu@gmail.com>
2025-12-02 21:25:06 -0500

    feat: created custom tab bar widget

--- lib/general_utils.dart
+++ lib/general_utils.dart
@@ -2,3 +2,4 @@ export 'src/enum_extensions.dart';
 export 'src/format.dart';
 export 'src/text_scale_box.dart';
 export 'src/grab_bar.dart';
+export 'src/custom_tab_bar.dart';
---
+++ lib/src/custom_tab_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'format.dart';
+
+class CustomTabBar extends StatefulWidget {
+  final List<Widget> tabs;
+  final Color? labelColor;
+  final Color? unselectedLabelColor;
+  final Color? indicatorColor;
+  final double? indicatorWeight;
+  final TabBarIndicatorSize? indicatorSize;
+  final TextStyle? labelStyle;
+  final Function(int)? onTap;
+  const CustomTabBar({
+    super.key,
+    required this.tabs,
+    this.labelColor,
+    this.unselectedLabelColor,
+    this.indicatorColor,
+    this.indicatorWeight,
+    this.indicatorSize,
+    this.labelStyle,
+    this.onTap,
+  });
+
+  @override
+  State<CustomTabBar> createState() => _CustomTabBarState();
+}
+
+class _CustomTabBarState extends State<CustomTabBar> {
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      onTap: widget.onTap,
+      labelColor: widget.labelColor ?? Colors.black,
+      unselectedLabelColor: widget.unselectedLabelColor ?? Colors.grey,
+      indicatorColor:
+          widget.indicatorColor ?? Colors.black.withValues(alpha: 0.5),
+      indicatorWeight: widget.indicatorWeight ?? 3,
+      indicatorSize: widget.indicatorSize ?? TabBarIndicatorSize.label,
+      splashBorderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(30),
+        topRight: Radius.circular(30),
+      ),
+      enableFeedback: true,
+      labelStyle: widget.labelStyle ?? roboto,
+      tabs: widget.tabs,
+    );
+  }
+}
